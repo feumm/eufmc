0b3467addce5892bdf69b40544bc38b1181f9b55
eufm <hundredrackies@gmail.com>
2026-06-24 15:04:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -188,4 +188,14 @@ router.post("/order", async (req, res) => {
   }
 });
 
+import express from "express";
+const app = express();
+app.use(express.json());
+app.use(router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 export default router;
